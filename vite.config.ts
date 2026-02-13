570fa8cd883e105b4ba7a3b7ce945e91cbeb8292
Fujiwara Tofu Shop <fujiwara@foundnone.xyz>
2026-02-13 13:33:22 -0800

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
-  base: './',
+  base: '/poop-fling/',
   server: { port: 3000, open: true },
   build: { outDir: 'dist' },
 });
